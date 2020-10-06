b00d515ba79b55b2d0a574aeb33c6be1ef58e8f4
jmota17 <jmotam@miumg.edu.gt>
2020-10-06 13:48:56 -0600

    Mantenimiento Pedido

---
+++ Prototipo2P/Prototipo2P/CapaControlador/Controlador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControlador
+{
+    public class Controlador
+    {
+
+        Sentencias sn = new Sentencias();
+
+        // LLenar una tabla CAPA CONTROLADOR
+        public DataTable llenarTbl(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenarTbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public void insertar(int em, int po, int bo, int ca, String fe) {
+            sn.insercion(em, po, bo, ca, fe);
+        }
+
+    }
+}
---
+++ Prototipo2P/Prototipo2P/CapaVista/MantenimientoPedido.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class MantenimientoPedido : Form
+    {
+        String ped = "tbl_pedido";
+        Controlador cn = new Controlador();
+
+        public MantenimientoPedido()
+        {
+            InitializeComponent();
+        }
+
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTbl(ped);
+            Dgv_registros.DataSource = dt;
+
+        }
+
+        private void Btn_Consultar_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_agregar_Click(object sender, EventArgs e)
+        {
+            int cant;
+            int emp;
+            int bod;
+            int prod;
+            String fecha="";
+
+            cant = int.Parse(txt_cantidad.Text);
+            emp = int.Parse(txt_empleado.Text);
+            bod = int.Parse(txt_bodega.Text);
+            prod = int.Parse(txt_producto.Text);
+            fecha = txt_fecha.Text;
+
+            cn.insertar(cant,emp,bod,prod,fecha);
+        }
+    }
+}
