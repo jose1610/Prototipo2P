54241ae42b57d58ed42423ca5fb4059cc3fe6930
jmota17 <jmotam@miumg.edu.gt>
2020-10-06 12:55:45 -0600

    Contenedores

--- Prototipo2P/CapaControlador/Controlador.cs
+++ Prototipo2P/CapaControlador/Controlador.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
 
 namespace CapaControlador
 {
-    class Controlador
+    public class Controlador
     {
+
+        Sentencias sn = new Sentencias();
+
+        // LLenar una tabla CAPA CONTROLADOR
+        public DataTable llenarTbl(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenarTbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
     }
 }
--- Prototipo2P/CapaModelo/Sentencias.cs
+++ Prototipo2P/CapaModelo/Sentencias.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
 
 namespace CapaModelo
 {
-    class Sentencias
+    public class Sentencias
     {
+
+        Conexion con = new Conexion();
+
+        public OdbcDataAdapter llenarTbl(string tabla)// metodo  que obtinene el contenio de una tabla
+        {
+            //string para almacenar los campos de OBTENERCAMPOS y utilizar el 1ro
+            string sql = "SELECT * FROM " + tabla + "  ;";
+            OdbcDataAdapter dataTable = new OdbcDataAdapter(sql, con.conexion());
+            return dataTable;
+        }
+
     }
 }
